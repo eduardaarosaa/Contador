25e1bff0f052120a7fae54dbad111c434cc3b962
eduardaarosaa <eduardacirina@gmail.com>
2019-06-19 13:56:28 -0300

    Pronto echo "# Contador" >> README.md git init git add README.md git commit -m "first commit" git remote add origin https://github.com/eduardaarosaa/Contador.git git push -u origin master

--- script/script.js
+++ script/script.js
@@ -1,20 +1,37 @@
-function tabuada() {
-    let num = document.getElementById("txt1");
-    let tabuada = document.getElementById('seltab');
-    if (num.value.length == 0) {
-        window.alert("Por favor, digite um número!");
+function contar() {
+    var inicio = document.getElementById("inicio");
+    var fim = document.getElementById("fim");
+    var passo = document.getElementById("passo");
+    var res = document.getElementById("res");
+
+    if (inicio.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
+        window.alert("Erro!! é necessário preencher os campos");
+        res.innerHTML = "Impossivel contar!!!";
     } else {
-        let n = Number(num.value);
-        let c = 1;
-        //Limpa para mostrar outra tabuada
-        tabuada.innerHTML = "";
-        while(c<=10){
-            let item = document.createElement('option');
-            item.text = `${n} x ${c} = ${n*c} `;
-            tabuada.appendChild(item);
-            c++;
+        res.innerHTML = 'Contando ... <br>';
+        let i = Number(inicio.value);
+        let f = Number(fim.value);
+        let p = Number(passo.value);
+        if (p <= 0) {
+            window.alert("Passo inválido!!! Será considerado o PASSO 1");
+            p = 1;
         }
+        if (i < f) {
 
+
+            for (let c = i; c <= f; c += p) {
+
+                res.innerHTML += `${c} \u{1F603}`;
+            }
+            //Unificado no JS é \u.
+        } else {
+            for (let c = i; c >= f; c -= p) {
+
+                res.innerHTML += `${c} \u{1F603}`;
+
+            }
+
+        }
+        res.innerHTML += `${c} \u{1F603}`;
     }
-
 }
